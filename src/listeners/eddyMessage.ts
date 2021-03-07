2de5c9993c63704f820d0dc82d81cd7d7903832e
loockeeer <lulu.r1313@gmail.com>
2021-03-07 14:31:32 +0100

    feat: add message listener for autoTalk, replies and channel links

---
+++ src/listeners/eddyMessage.ts
@@ -0,0 +1,43 @@
+import * as app from "../app"
+
+const listener: app.Listener<"message"> = {
+  event: "message",
+  async run(message) {
+    if(!app.isCommandMessage(message)) return
+    if(message.author.bot) return
+
+    const prefix = await app.prefix(message.guild ?? undefined)
+
+    if (message.content.startsWith(prefix)) return
+
+    try {
+      if (!message.client.user) return
+      if (await app.isReferencedAnswer(message, message.client.user.id) && message.guild) {
+        const autoTalk = app.eddy.getAutoTalk(message.guild)
+        if (autoTalk) {
+          const dataset = new app.eddy.Dataset(autoTalk.datasetName)
+          app.reply(app.eddy.generate(dataset, message.content, message), message)
+        }
+      }
+
+      const link = app.eddy.getLink(message.channel)
+      if (link) {
+        const dataset = new app.eddy.Dataset(link)
+        app.reply(app.eddy.generate(dataset, message.content, message), message)
+      }
+
+      if (!message.guild) return
+      const autoTalk = app.eddy.getAutoTalk(message.guild)
+      if (autoTalk) {
+        if (Math.random() < autoTalk.probability) {
+          const dataset = new app.eddy.Dataset(autoTalk.datasetName)
+          app.reply(app.eddy.generate(dataset, message.content, message), message)
+        }
+      }
+    } catch {
+      app.reply("gneuh t'as pas la perm pour m'utiliser ducon", message)
+    }
+  }
+}
+
+module.exports = listener
