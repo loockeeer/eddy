870d1dd64eb478286c9665208c49aa3acb2b2fb9
loockeeer <lulu.r1313@gmail.com>
2021-03-09 15:48:25 +0100

    feat: add dynamic bot invite generation

--- src/commands/stats.ts
+++ src/commands/stats.ts
@@ -5,9 +5,12 @@ import readableSize from "readable-size"
 const command: app.Command = {
   name: "stats",
   async run(message) {
+    const invite = await message.client.generateInvite({
+      permissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+    })
     const embed = new app.MessageEmbed()
       .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
-      .setDescription("I'm a dumb discord bot you can train by speaking with me.")
+      .setDescription(`I'm a dumb discord bot you can train by speaking with me. [Invite me](${invite}) !`)
       .addField('Owner', (await message.client.users.fetch(process.env.OWNER as string)).tag, true)
       .addField('Websocket Ping', `${message.client.ws.ping} ms`, true)
       .addField('Dataset Count', app.datasets.count, true)
