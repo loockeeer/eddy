a64a989b4995be7de840f271c9c8a897609a529e
loockeeer <lulu.r1313@gmail.com>
2021-05-27 23:31:12 +0200

    fix: fix chart argument

--- src/commands/chart.ts
+++ src/commands/chart.ts
@@ -6,14 +6,13 @@ const command: app.Command = {
   positional: [
     {
       name: "choice",
-      checkValue: /send|accept|decline/,
       checkValueError: "You need to enter either send, accept or decline"
     }
   ],
   async run(message) {
     switch(message.positional.choice) {
       case 'send':
-        await message.react('�');
+        await message.react('📨');
         await sendChart(message, message.author);
         break;
       case 'accept':
@@ -30,6 +29,9 @@ const command: app.Command = {
         })
         await message.channel.send(app.messageEmbed('You declined the chart', message.author))
         break;
+      default:
+        const {accepted} = app.database.userVerification.ensure(message.author.id, {chartSent: false, accepted: false})
+        await message.channel.send(app.messageEmbed(`You have currently ${accepted ? 'accepted' : 'declined'} the chart.`, message.author))
     }
   }
 }
